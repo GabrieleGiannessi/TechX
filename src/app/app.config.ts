a48a89706cda321175670af9d6e373e670b1b354
Gabriele <gabrielegiannessi@gmail.com>
2024-08-29 13:32:54 +0200

    aggiunta cartella services e modificati i providers nel file config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,28 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfLUXCZ8C8k4KgAGAO5sHrFSdDBtkboc8",
+  authDomain: "app-saw2024.firebaseapp.com",
+  projectId: "app-saw2024",
+  storageBucket: "app-saw2024.appspot.com",
+  messagingSenderId: "151881399800",
+  appId: "1:151881399800:web:c13ea3f9b7f1777148aa0d",
+  measurementId: "G-6Y8KGCYC2J"
+};
+
+// Initialize Firebase
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
     provideClientHydration(), 
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
