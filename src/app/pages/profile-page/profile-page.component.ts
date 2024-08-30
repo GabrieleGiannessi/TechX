182860e61663a50d1e96c80c977074f8b094a4e2
Gabriele <gabrielegiannessi@gmail.com>
2024-08-30 16:41:23 +0200

    profile page inizializzata

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,13 +1,15 @@
-import { Component, computed, effect, ElementRef, inject, input, ViewChild } from '@angular/core';
+import { Component, computed, effect, ElementRef, inject, input, signal, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FirestoreService } from '../../services/firestore.service';
-import { computeMsgId } from '@angular/compiler';
 import { StorageService } from '../../services/storage.service';
+import { Router } from '@angular/router';
+import { FilterButtonComponent } from "../../components/filter-button/filter-button.component";
+import { ArticleComponent } from "../../components/article/article.component";
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [FilterButtonComponent, ArticleComponent],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
@@ -16,12 +18,19 @@ export class ProfilePageComponent {
   authService = inject (AuthService); 
   firestore = inject (FirestoreService); 
   storage = inject (StorageService); 
+  router = inject (Router)
 
   @ViewChild('profileImage') profileImage! : ElementRef<HTMLInputElement>; 
 
   id = input.required<string>(); 
 
+  state = signal<string>(''); 
   user = computed (() => this.firestore.users().find ((user) => user.uid === this.id())); 
+
+  userArticles = computed (() => {
+    if (this.state()) return this.firestore.articles().filter( (article) => article.userID === this.id()); 
+    return this.firestore.articles().filter( (article) => article.userID === this.id() && article.state === this.state());
+  } )
   
   constructor(){
     effect (() => {
