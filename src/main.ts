7bc22bdf9abf60d0b5610782120c901f36733917
Gabriele <gabrielegiannessi@gmail.com>
2024-09-02 00:33:22 +0200

    service worker messo

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener("load", () => {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("service-worker.js");
+      }
+    });
+}
