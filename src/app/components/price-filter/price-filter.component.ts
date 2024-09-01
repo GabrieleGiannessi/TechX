44c281e58f1d63c8167375634d855137e9450836
Gabriele <gabrielegiannessi@gmail.com>
2024-09-01 16:45:45 +0200

    Introdotti filtri per gli articoli

--- src/app/components/price-filter/price-filter.component.ts
+++ src/app/components/price-filter/price-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 
 @Component({
   selector: 'app-price-filter',
@@ -9,4 +9,64 @@ import { Component } from '@angular/core';
 })
 export class PriceFilterComponent {
 
+  max = model.required<number>();
+  min = model.required<number>();
+
+  onChangeValue() {
+    const inputElements = document.querySelectorAll("input");
+
+    inputElements.forEach(element => {
+      element.addEventListener("input", () => {
+        this.validateRange();
+      })
+    })
+  }
+
+  validateRange() {
+    let minValue = document.querySelector('#min-value');
+    let maxValue = document.querySelector('#max-value');
+
+    let rangeFill = <HTMLElement>document.querySelector('.range-fill');
+    const inputElements = document.querySelectorAll("input");
+
+    let minPrice = parseInt(inputElements[0].value);
+    let maxPrice = parseInt(inputElements[1].value);
+
+    if (minPrice > maxPrice) {
+      let temp = maxPrice;
+      maxPrice = minPrice;
+      minPrice = temp;
+    }
+
+    const minPercentage = ((minPrice - 10) / 9989) * 100;
+    const maxPercentage = ((maxPrice - 10) / 9989) * 100;
+
+    rangeFill!.style.left = minPercentage + '%';
+    rangeFill!.style.width = maxPercentage - minPercentage + '%';
+
+    minValue!.innerHTML = minPrice + '€';
+    maxValue!.innerHTML = maxPrice + "€";
+  }
+
+  updateRange(minValue: number, maxValue: number) {
+    if (minValue > maxValue) {
+      [minValue, maxValue] = [maxValue, minValue]; // Swap values if min > max
+    }
+
+    this.min.set(minValue);
+    this.max.set(maxValue);
+  }
+
+  onMinInput(e : Event) {
+    const inputElement = <HTMLInputElement> e.target ;
+    const minValue = inputElement.valueAsNumber;
+    this.updateRange(minValue, this.max());
+  }
+
+  onMaxInput(e : Event) {
+    const inputElement = <HTMLInputElement> e.target;
+    const maxValue = inputElement.valueAsNumber;
+    this.updateRange(this.min(), maxValue);
+  }
 }
+
--- src/app/pages/articles-page/articles-page.component.ts
+++ src/app/pages/articles-page/articles-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, effect, inject, model, signal, ViewChild } from '@angular/core';
+import { Component, computed, effect, inject, model, OnInit, signal, ViewChild } from '@angular/core';
 import { FilterButtonComponent } from "../../components/filter-button/filter-button.component";
-import { FirestoreService } from '../../services/firestore.service';
+import { Article, FirestoreService } from '../../services/firestore.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ArticleComponent } from "../../components/article/article.component";
 import { AuthService } from '../../services/auth.service';
@@ -8,17 +8,18 @@ import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 import { debounceTime, distinctUntilChanged, filter, map, merge, Observable, OperatorFunction, Subject } from 'rxjs';
 import { CategoryFilterComponent } from "../../components/category-filter/category-filter.component";
 import { OrderFilterComponent } from "../../components/order-filter/order-filter.component";
+import { PriceFilterComponent } from "../../components/price-filter/price-filter.component";
 
 
 
 @Component({
   selector: 'app-articles-page',
   standalone: true,
-  imports: [FilterButtonComponent, ArticleComponent, CategoryFilterComponent, OrderFilterComponent],
+  imports: [FilterButtonComponent, ArticleComponent, CategoryFilterComponent, OrderFilterComponent, PriceFilterComponent],
   templateUrl: './articles-page.component.html',
   styleUrl: './articles-page.component.css'
 })
-export class ArticlesPageComponent {
+export class ArticlesPageComponent implements OnInit {
 
   firestore = inject (FirestoreService)
   router = inject (Router)
@@ -28,26 +29,47 @@ export class ArticlesPageComponent {
   title = signal<string>(''); 
   category = signal<string>(''); 
   order = signal<string>(''); 
+  max = signal<number>(9999);
+  min = signal<number>(0);
 
-  constructor(){
-    if (this.route.snapshot.queryParams['title']) this.title.set(this.route.snapshot.queryParams['title']);
-    if (this.route.snapshot.queryParams['category'])  this.category.set(this.route.snapshot.queryParams['category']);
-    if (this.route.snapshot.queryParams['order'])  this.order.set(this.route.snapshot.queryParams['order']);
+  log = effect (() => console.log (this.category())); 
+
+  articles = signal <Article[]> ( this.firestore.articles() ); 
+  dbArticles = computed (() => this.articles()); 
+
+  ngOnInit () : void {
+    this.route.queryParamMap.subscribe(params => {
+      const titleParam = params.get('title');
+      const categoryParam = params.get('category');
+      const orderParam = params.get("order");
+      const priceMaxParam = params.get('maxprice'); 
+      const priceMinParam = params.get("minprice");
+
+      if(titleParam) this.title.set(titleParam);
+
+      this.filter(titleParam, categoryParam, orderParam, priceMaxParam, priceMinParam );
+      });
+  }
+  filter(title: string | null, category: string | null, order: string | null, max: string | null, min: string | null ) {
     
-    effect (() => {
-      console.log (this.title())
-      console.log (this.order())
-      console.log (this.category())      
-    })
+    if (title) this.articles.set ( this.articles().filter(article => article.title.toLowerCase().includes(title.toLowerCase()))); 
+    if (category) this.articles.set ( this.articles().filter (article => article.category === category)); 
+    if (order){
+      if(order === 'ASC') this.articles.set(this.articles().sort((a, b) => a.price - b.price));
+      if (order === 'DESC') this.articles.set(this.articles().sort((a, b) => b.price - a.price));
+    }
+    if (max && min) this.articles.set (this.articles().filter((article) => article.price <= this.max() && article.price >= this.min()));
   }
 
   onFormSubmit() {
-    console.log (this.title(), this.order(), this.category()); 
     this.router.navigate(['articles'], {queryParams : {
       title : this.title(),
       category : this.category(), 
       order : this.order(),
-    }
+      max : this.max(),
+      min : this.min()
+    }, 
+    queryParamsHandling: 'merge'
     }); 
     }
 }
